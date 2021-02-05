92b8bad77e75322eafe8d3979108db4b91b26bf5
Chanhee Jeong <chan420420@gmail.com>
2021-02-05 16:48:39 +0900

    마지막에서 처음으로

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -31,18 +31,28 @@ function Slider({slides, autoPlay}: Props) {
     /* 이미지 넓이 : 현재는 전체화면이라 윈도우 전체넓이 인듯? 나중에 props로 처리해야할듯함  */
     const getWidth = () => window.innerWidth
 
+    const firstSlide = slides[0]
+    const secondSlide = slides[1]
+    const lastSlide = slides[slides.length - 1]
+
     const [state, setState] = useState({
         activeSlide: 0,
         translate: 0,
-        transition: 0.45
+        transition: 0.45,
+        _slides: [lastSlide, firstSlide, secondSlide]
     })
 
     const { translate, transition, activeSlide } = state
 
     const autoPlayRef = useRef<() => void>(() => {})
+    // const transitionRef = useRef<() => void>(() => {})
+    const resizeRef = useRef<() => void>(() => {})
 
-    useEffect(()  => {
+
+    useEffect(() => {
         autoPlayRef.current = nextSlide
+        // transitionRef.current = smoothTransition
+        resizeRef.current = handleResize
     })
 
     useEffect(() => {
@@ -53,6 +63,70 @@ function Slider({slides, autoPlay}: Props) {
         return () => clearInterval(interval)
     }, [])
 
+
+
+    // useEffect(() => {
+    //     autoPlayRef.current = nextSlide
+    //     transitionRef.current = smoothTransition
+    // })
+
+    useEffect(() => {
+
+        const play = () => {
+            autoPlayRef.current()
+        }
+
+        // const smooth = () => {
+        //     transitionRef.current()
+        // }
+
+        const resize = () => {
+            resizeRef.current()
+        }
+
+        const interval = setInterval(play, autoPlay * 1000)
+        // const transitionEnd = window.addEventListener('transitionend', smooth)
+        const onResize = window.addEventListener('resize', resize)
+
+        return () => {
+            clearInterval(interval)
+            // window.removeEventListener('transitionend', smooth)
+            window.removeEventListener('resize', resize)
+        }
+
+    }, [])
+
+
+
+
+    useEffect(() => {
+        if (transition === 0) setState({ ...state, transition: 0.45 })
+    }, [transition])
+
+
+
+    const smoothTransition = () => {
+        let _slides = []
+
+        // We're at the last slide.
+        if (activeSlide === slides.length - 1)
+            _slides = [slides[slides.length - 2], lastSlide, firstSlide]
+        // We're back at the first slide. Just reset to how it was on initial render
+        else if (activeSlide === 0) _slides = [lastSlide, firstSlide, secondSlide]
+        // Create an array of the previous last slide, and the next two slides that follow it.
+        else _slides = slides.slice(activeSlide - 1, activeSlide + 2)
+
+        setState({
+            ...state,
+            _slides,
+            transition: 0,
+            translate: getWidth()
+        })
+    }
+
+    const handleResize = () => {
+        setState({ ...state, translate: getWidth(), transition: 0 })
+    }
 
 
     const nextSlide = () => {
